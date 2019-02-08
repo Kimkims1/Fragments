f8b9efc0783b801f78625d541808535d3d14a618
Mullatoez <kimanthikevin76@gmail.com>
2019-02-07 17:43:48 -0800

    Fragments

---
+++ app/src/main/java/com/example/bottomnav/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.bottomnav;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.slip1:
+                            selectedFragment = new SlipOne();
+                            break;
+
+                        case R.id.slip2:
+                            selectedFragment = new SlipTwo();
+                            break;
+
+                        case R.id.slip3:
+                            selectedFragment = new SlipThree();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
